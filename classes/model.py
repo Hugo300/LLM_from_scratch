a75e85d632695c3d03ea733a5f416e06bc868a90
Hugo Ferreira <hugo.ferreira3000@gmail.com>
2025-07-09 16:46:29 +0100

    added method to gptmodel class to directly load gpt weights

--- classes/model.py
+++ classes/model.py
@@ -1,8 +1,18 @@
 import torch
 import torch.nn as nn
+import numpy as np
 
 from classes.attention import MultiHeadAttention
 from classes.activation import GELU
+
+from tools.gpt_downloader import download_and_load_gpt2
+
+
+def assign(left, right):
+    if left.shape != right.shape:
+        raise ValueError(f"Size mismatch: {left.shape} - {right.shape}")
+    
+    return torch.nn.Parameter(torch.tensor(right))
 
 
 class GPTModel(nn.Module):
@@ -19,6 +29,9 @@ class GPTModel(nn.Module):
 
         self.final_norm = LayerNormalization(cfg["emb_dim"])
         self.output_layer = nn.Linear(cfg['emb_dim'], cfg["vocab_size"], bias=False)
+
+        self.gpt_model_name = cfg["gpt_model_name"]
+        self.gpt_num_params = cfg["gpt_num_params"]
     
     def forward(self, in_indexes):
         bacth_size, sequence_shape = in_indexes.shape
@@ -39,6 +52,68 @@ class GPTModel(nn.Module):
     
     def load_weights(self, filepath, device):
         self.load_state_dict(torch.load(filepath, map_location=device))
+
+    def load_weights_gpt(self):
+        settings, params = download_and_load_gpt2(model_size=self.gpt_num_params, models_dir="gpt2")
+
+        self.position_emb.weight = assign(self.position_emb.weight, params['wpe'])
+        self.token_emb.weight = assign(self.token_emb.weight, params['wte'])
+        
+        for b in range(len(params["blocks"])):
+            q_w, k_w, v_w = np.split(
+                (params["blocks"][b]["attn"]["c_attn"])["w"], 3, axis=-1)
+            self.trf_blocks[b].attention.W_queries.weight = assign(
+                self.trf_blocks[b].attention.W_queries.weight, q_w.T)
+            self.trf_blocks[b].attention.W_keys.weight = assign(
+                self.trf_blocks[b].attention.W_keys.weight, k_w.T)
+            self.trf_blocks[b].attention.W_values.weight = assign(
+                self.trf_blocks[b].attention.W_values.weight, v_w.T)
+
+            q_b, k_b, v_b = np.split(
+                (params["blocks"][b]["attn"]["c_attn"])["b"], 3, axis=-1)
+            self.trf_blocks[b].attention.W_queries.bias = assign(
+                self.trf_blocks[b].attention.W_queries.bias, q_b)
+            self.trf_blocks[b].attention.W_keys.bias = assign(
+                self.trf_blocks[b].attention.W_keys.bias, k_b)
+            self.trf_blocks[b].attention.W_values.bias = assign(
+                self.trf_blocks[b].attention.W_values.bias, v_b)
+
+            self.trf_blocks[b].attention.out_proj.weight = assign(
+                self.trf_blocks[b].attention.out_proj.weight, 
+                params["blocks"][b]["attn"]["c_proj"]["w"].T)
+            self.trf_blocks[b].attention.out_proj.bias = assign(
+                self.trf_blocks[b].attention.out_proj.bias, 
+                params["blocks"][b]["attn"]["c_proj"]["b"])
+
+            self.trf_blocks[b].fforward.layers[0].weight = assign(
+                self.trf_blocks[b].fforward.layers[0].weight, 
+                params["blocks"][b]["mlp"]["c_fc"]["w"].T)
+            self.trf_blocks[b].fforward.layers[0].bias = assign(
+                self.trf_blocks[b].fforward.layers[0].bias, 
+                params["blocks"][b]["mlp"]["c_fc"]["b"])
+            self.trf_blocks[b].fforward.layers[2].weight = assign(
+                self.trf_blocks[b].fforward.layers[2].weight, 
+                params["blocks"][b]["mlp"]["c_proj"]["w"].T)
+            self.trf_blocks[b].fforward.layers[2].bias = assign(
+                self.trf_blocks[b].fforward.layers[2].bias, 
+                params["blocks"][b]["mlp"]["c_proj"]["b"])
+
+            self.trf_blocks[b].norm_1.scale = assign(
+                self.trf_blocks[b].norm_1.scale, 
+                params["blocks"][b]["ln_1"]["g"])
+            self.trf_blocks[b].norm_1.shift = assign(
+                self.trf_blocks[b].norm_1.shift, 
+                params["blocks"][b]["ln_1"]["b"])
+            self.trf_blocks[b].norm_2.scale = assign(
+                self.trf_blocks[b].norm_2.scale, 
+                params["blocks"][b]["ln_2"]["g"])
+            self.trf_blocks[b].norm_2.shift = assign(
+                self.trf_blocks[b].norm_2.shift, 
+                params["blocks"][b]["ln_2"]["b"])
+
+        self.final_norm.scale = assign(self.final_norm.scale, params["g"])
+        self.final_norm.shift = assign(self.final_norm.shift, params["b"])
+        self.output_layer.weight = assign(self.output_layer.weight, params["wte"])
 
 
 class LayerNormalization(nn.Module):
