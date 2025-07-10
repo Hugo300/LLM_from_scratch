2409cc6a115cd07b67111c982bd81d98b0c721d9
Hugo Ferreira <hugo.ferreira3000@gmail.com>
2025-07-10 11:47:51 +0100

    created file to hold the diff dataset classes

---
+++ classes/dataset.py
@@ -0,0 +1,108 @@
+import torch
+from torch.utils.data import Dataset
+
+import pandas as pd
+
+
+# fundational LLM model dataset
+class LLMDataset(Dataset):
+    def __init__(self, text, tokenizer, max_length, stride):
+        self.input_ids = []
+        self.target_ids = []
+
+        token_ids = tokenizer.encode(text, allowed_special={"<|endoftext|>"})
+
+        for i in range(0, len(token_ids) - max_length, stride):
+            input_chunk = token_ids[i:i+max_length]
+            target_chunk = token_ids[i+1:i+max_length+1]
+
+            self.input_ids.append(torch.tensor(input_chunk))
+            self.target_ids.append(torch.tensor(target_chunk))
+
+    def __len__(self):
+        return len(self.input_ids)
+    
+    def __getitem__(self, index):
+        return self.input_ids[index], self.target_ids[index]
+    
+
+# dataset for classification llm (specificaly for spam detection)
+class SpamDataset(Dataset):
+    def __init__(self, csv_file, tokenizer, max_length=None, pad_token_id=50256):
+        self.data = pd.read_csv(csv_file)
+
+        # Pre-tokenize texts
+        self.encoded_texts = [
+            tokenizer.encode(text) for text in self.data["Text"]
+        ]
+
+        if max_length is None:
+            self.max_length = self._longest_encoded_length()
+        else:
+            self.max_length = max_length
+            # Truncate sequences if they are longer than max_length
+            self.encoded_texts = [
+                encoded_text[:self.max_length]
+                for encoded_text in self.encoded_texts
+            ]
+
+        # Pad sequences to the longest sequence
+        self.encoded_texts = [
+            encoded_text + [pad_token_id] * (self.max_length - len(encoded_text))
+            for encoded_text in self.encoded_texts
+        ]
+
+    def __getitem__(self, index):
+        encoded = self.encoded_texts[index]
+        label = self.data.iloc[index]["Label"]
+        return (
+            torch.tensor(encoded, dtype=torch.long),
+            torch.tensor(label, dtype=torch.long)
+        )
+
+    def __len__(self):
+        return len(self.data)
+
+    def _longest_encoded_length(self):
+        max_length = 0
+        for encoded_text in self.encoded_texts:
+            encoded_length = len(encoded_text)
+            if encoded_length > max_length:
+                max_length = encoded_length
+        return max_length
+        # Note: A more pythonic version to implement this method
+        # is the following, which is also used in the next chapter:
+        # return max(len(encoded_text) for encoded_text in self.encoded_texts)
+
+
+# dataset for llm assistant (instruction following)
+class InstructionDataset(Dataset):
+    def __init__(self, data, tokenizer):
+        self.data = data
+        
+        self.encoded_text = []
+        for entry in data:
+            boiler_p_instruction_p_input = self.format_input(entry)
+            response = f"\n\n### Response:\n{entry['output']}"
+
+            self.encoded_text.append(
+                tokenizer.encode(boiler_p_instruction_p_input + response)
+            )
+
+    # this is based on a older, more widespread approach
+    def format_input(entry):
+        instruction_text = (
+            f"Below is an instruction that describes a task. "
+            f"Write a response that appropriately completes the request."
+            f"\n\n### Instruction:\n{entry['instruction']}"
+        )
+
+        input_text = f"\n\n### Input\n{entry['input']}" if entry['input'] else ""
+
+        return instruction_text + input_text
+    
+    def __getitem__(self, index):
+        return self.encoded_text[index]
+    
+    def __len__(self):
+        return len(self.data)
