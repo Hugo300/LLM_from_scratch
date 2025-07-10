042243503f890407a03b6d75bd8bd0587750497e
Hugo Ferreira <hugo.ferreira3000@gmail.com>
2025-07-10 11:48:15 +0100

    Removed dataset classes from the dataloader file

--- classes/dataloader.py
+++ classes/dataloader.py
@@ -1,27 +1,7 @@
-import torch
-from torch.utils.data import Dataset, DataLoader
-import pandas as pd
+from torch.utils.data import DataLoader
 
-class LLMDataset(Dataset):
-    def __init__(self, text, tokenizer, max_length, stride):
-        self.input_ids = []
-        self.target_ids = []
+from classes.dataset import LLMDataset    
 
-        token_ids = tokenizer.encode(text, allowed_special={"<|endoftext|>"})
-
-        for i in range(0, len(token_ids) - max_length, stride):
-            input_chunk = token_ids[i:i+max_length]
-            target_chunk = token_ids[i+1:i+max_length+1]
-
-            self.input_ids.append(torch.tensor(input_chunk))
-            self.target_ids.append(torch.tensor(target_chunk))
-
-    def __len__(self):
-        return len(self.input_ids)
-    
-    def __getitem__(self, index):
-        return self.input_ids[index], self.target_ids[index]
-    
 
 def create_dataloader(
         text, tokenizer, batch_size=4, max_length=256, 
@@ -44,49 +24,3 @@ def create_dataloader(
     return dataloader
 
 
-class SpamDataset(Dataset):
-    def __init__(self, csv_file, tokenizer, max_length=None, pad_token_id=50256):
-        self.data = pd.read_csv(csv_file)
-
-        # Pre-tokenize texts
-        self.encoded_texts = [
-            tokenizer.encode(text) for text in self.data["Text"]
-        ]
-
-        if max_length is None:
-            self.max_length = self._longest_encoded_length()
-        else:
-            self.max_length = max_length
-            # Truncate sequences if they are longer than max_length
-            self.encoded_texts = [
-                encoded_text[:self.max_length]
-                for encoded_text in self.encoded_texts
-            ]
-
-        # Pad sequences to the longest sequence
-        self.encoded_texts = [
-            encoded_text + [pad_token_id] * (self.max_length - len(encoded_text))
-            for encoded_text in self.encoded_texts
-        ]
-
-    def __getitem__(self, index):
-        encoded = self.encoded_texts[index]
-        label = self.data.iloc[index]["Label"]
-        return (
-            torch.tensor(encoded, dtype=torch.long),
-            torch.tensor(label, dtype=torch.long)
-        )
-
-    def __len__(self):
-        return len(self.data)
-
-    def _longest_encoded_length(self):
-        max_length = 0
-        for encoded_text in self.encoded_texts:
-            encoded_length = len(encoded_text)
-            if encoded_length > max_length:
-                max_length = encoded_length
-        return max_length
-        # Note: A more pythonic version to implement this method
-        # is the following, which is also used in the next chapter:
-        # return max(len(encoded_text) for encoded_text in self.encoded_texts)
